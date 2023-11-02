ce79b98e0ba3412e0367c59470c7100852a504e5
Dominic <domcashiola@gmail.com>
2023-11-02 19:28:19 -0400

    homework for w3d5

---
+++ practice-for-ch-ruby-abstract-data-types-main/queue.rb
@@ -0,0 +1,20 @@
+class Queue
+  attr_reader :array
+  def initialize
+    @array = []
+  end
+
+  def enqueue(el)
+    array.push(el)
+  end
+
+  def dequeue
+    array.shift
+  end
+
+  def peek
+    array.first
+  end
+
+  private
+end
